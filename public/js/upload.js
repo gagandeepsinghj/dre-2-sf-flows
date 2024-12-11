4b191470130b7a1e767db29e5cedc5319282f1f6
Gagan Singh <gagan.singh@claritisoftware.com>
2024-12-10 23:20:32 -0500

    Format XML output

--- public/js/upload.js
+++ public/js/upload.js
@@ -118,8 +118,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Show and update the output textarea and deploy button
             outputContent.classList.remove('hidden');
             deployBtn.classList.remove('hidden');
-            outputContent.value = JSON.stringify(data.flowContent, null, 2);
 
+            outputContent.value = prettifyXml(data.flowContent);
         } catch (error) {
             alert('Error during conversion: ' + error.message);
         } finally {
@@ -127,6 +127,29 @@ document.addEventListener('DOMContentLoaded', () => {
             convertBtn.disabled = false;
         }
     }
+
+    function prettifyXml(sourceXml) {
+        var xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
+        var xsltDoc = new DOMParser().parseFromString([
+        // describes how we want to modify the XML - indent everything
+            '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+            '  <xsl:strip-space elements="*"/>',
+            '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
+            '    <xsl:value-of select="normalize-space(.)"/>',
+            '  </xsl:template>',
+            '  <xsl:template match="node()|@*">',
+            '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+            '  </xsl:template>',
+            '  <xsl:output indent="yes"/>',
+            '</xsl:stylesheet>',
+        ].join('\n'), 'application/xml');
+
+        var xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(xsltDoc);
+        var resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+        var resultXml = new XMLSerializer().serializeToString(resultDoc);
+        return resultXml;
+    };
 
     async function handleFiles(files) {
         if (files.length > 0) {
